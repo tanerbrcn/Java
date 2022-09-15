3ac33d2807eb1c5d4d79b699ea7ecad8a983832a
taner.barcin <taner.barcin@ARGE28.Esit.local>
2022-09-15 17:15:42 +0300

    While loop and do-while loop example

---
+++ JavaLearning/src/Examples/While.java
@@ -0,0 +1,27 @@
+package Examples;
+
+public class While {
+
+	public static void main(String[] args) {
+		// While
+		// Şart sağlanmaz ise hiçbir zaman döngü çalışmaz.
+		int i = 1;
+		while (i <= 10) {
+			System.out.println("While Döngüsü : " + i);
+			i++;
+		}
+
+		System.out.println("-------------");
+
+		// Do-While
+		// Şart sağlanırsa while ile aynı işlemi yapar. Şart sağlanmasa bile döngü bir
+		// kez çalışır.
+		// Aşağıdaki örnek şartın sağlamadığı durumdur.
+		int j = 15;
+		do {
+			System.out.println("Do-While Döngüsü : " + j);
+			j++;
+		} while (j <= 10);
+
+	}
+}
