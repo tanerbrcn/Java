c29327a5b689a0294de81d2ddaf86bcec3a84469
taner.barcin <taner.barcin@ARGE28.Esit.local>
2022-09-23 11:32:58 +0300

    Inheritance example

---
+++ JavaLearning/src/OOP_Example/Inheritance/EmployeeManager.java
@@ -0,0 +1,11 @@
+package OOP_Example.Inheritance;
+
+public class EmployeeManager extends Employee{
+
+	public EmployeeManager(String name, String department, int salary) {
+		super(name, department, salary);
+		
+	
+	}
+
+}
---
+++ JavaLearning/src/OOP_Example/Inheritance/Main.java
@@ -0,0 +1,16 @@
+package OOP_Example.Inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*Employee employee = new Employee("Taner", "AR-GE", 50);
+		employee.showInfos();*/
+		
+		EmployeeManager employeeManager=new EmployeeManager("Taner","AR-GE",50);
+		employeeManager.showInfos();
+		
+		
+	}
+
+}
